6bcedcb7d2ca4db5a2cca788a45f23cb3ab16dc4
illonion <zhou.lawrence.yz@gmail.com>
2024-01-08 13:56:58 +1100

    Move components to different files/folders

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,6 @@
-import reactImg from './assets/react-core-concepts.png'
 import { CORE_CONCEPTS } from './data.js'
-
-const reactDescriptions = ['Fundamental', 'Crucial', 'Core']
-
-function getRandomInt(max) {
-  return Math.floor(Math.random() * (max + 1))
-}
-
-function Header() {
-  const description = reactDescriptions[getRandomInt(reactDescriptions.length - 1)]
-
-  return (
-    <header>
-      <img src={reactImg} alt="Stylized atom" />
-      <h1>React Essentials</h1>
-      <p>
-      {description} React concepts you will need for almost any app you are
-        going to build!
-      </p>
-    </header>
-  )
-}
-
-function CoreComponent(props) {
-  return (
-    <li>
-      <img src={props.image} alt={props.title} />
-      <h3>{props.title}</h3>
-      <p>{props.description}</p>
-    </li>
-  )
-}
+import Header from './components/Header.jsx'
+import CoreComponent from './components/CoreComponent.jsx'
 
 function App() {
   return (
